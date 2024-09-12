b35ef07fe247f02b3a73497882314198e2774a81
alexisalvz3 <alexis.alvz3@gmail.com>
2024-09-11 17:35:22 -0700

    Added name filter endpoint

--- ServerlessGOT-Knights-API/got_knights/app.py
+++ ServerlessGOT-Knights-API/got_knights/app.py
@@ -44,26 +44,32 @@ def get_knights(event, context):
     query_parameters = event.get('queryStringParameters', {})
 
     # Check if there's an allegiance query parameter
+    name = query_parameters.get('name') if query_parameters else None
     allegiance = query_parameters.get('allegiance') if query_parameters else None
 
     # error handling done in try-except block
     try:
-        if allegiance:
-            # grabbing list of knights with allegiance to user-specified House from dynamoDB
+        if name:            
+            response = table.get_item(Key={'name': name})           
+            # placing correct response into knights array
+            knights = [response['Item']] if 'Item' in response else []
+        elif allegiance:
             response = table.scan(
                 FilterExpression=boto3.dynamodb.conditions.Attr('allegiance').eq(allegiance)
-            )            # case where no knights found with allegiance to user-specified House
+            )
+            # placing correct response into knights array
+            knights = response['Items']
         else:
             response = table.scan()
+            # placing correct response into knights array
+            knights = response['Items']
         
-        # placing correct response into knights array
-        knights = response['Items']
         
         # case where we didnt find any knights with appropriate allegiance
         if not knights:
             return {
                 "statusCode": 404,
-                "body": json.dumps({"error": f"No knights found" + (f" with allegiance to {allegiance}" if allegiance else "")}), 
+                "body": json.dumps({"error": f"No knights found" + (f" with name {name}" if name else "") + (f" with allegiance to {allegiance}" if allegiance else "")}),                
                 "headers": {"Content-Type": "application/json"}
             }
         # case where we succesfully return list of knights with correct allegiance
